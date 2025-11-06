746a93b85480b61323936bc15e81698460873058
Adriaann <adriaannm.rodriguezr@uqvirtual.edu.co>
2025-11-06 10:15:41 -0500

    Modificación temporal del archivo src/app/app-routing-module.ts para probar Atomic

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./features/auth/auth-module').then((m) => m.AuthModule),
+  },
+  // Agregar solo para probar el sistema de diseño
+  {
+    path: 'atomic',
+    loadChildren: () => import('./atomic/atomic-module').then((m) => m.AtomicModule),
   },
 ];
 
