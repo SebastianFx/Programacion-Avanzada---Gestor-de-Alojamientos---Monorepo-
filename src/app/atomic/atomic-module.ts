a8860221bc37b54710f15be47948017e45f01c47
Adriaann <adriaannm.rodriguezr@uqvirtual.edu.co>
2025-11-07 12:46:54 -0500

    Login

--- src/app/atomic/atomic-module.ts
+++ src/app/atomic/atomic-module.ts
@@ -1,26 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
-
-// Angular Material Modules
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
-// Atomic Components
 import { ButtonComponent } from './atoms/button/button';
 import { InputComponent } from './atoms/input/input';
 import { LabelComponent } from './atoms/label/label';
+//import { IconComponent } from './1_atoms/icon/icon';
 import { FormFieldComponent } from './molecules/form-field/form-field';
 import { LoginFormComponent } from './organisms/login-form/login-form';
 import { AuthTemplateComponent } from './templates/auth-template/auth-template';
 import { DesignShowcaseComponent } from './pages/design-showcase/design-showcase';
 
-// Rutas internas del módulo atomic
+// Agregar solo para probar el sistema de diseño
+import { RouterModule, Routes } from '@angular/router';
 const atomicRoutes: Routes = [
   {
+    // Segmento restante: 'pages/design-showcase'
     path: 'pages/design-showcase',
     component: DesignShowcaseComponent,
   },
@@ -43,7 +42,7 @@ const atomicRoutes: Routes = [
     MatInputModule,
     MatIconModule,
     MatFormFieldModule,
-    RouterModule.forChild(atomicRoutes),
+    RouterModule.forChild(atomicRoutes), // Agregar solo para probar el sistema de diseño
   ],
   exports: [
     ButtonComponent,
